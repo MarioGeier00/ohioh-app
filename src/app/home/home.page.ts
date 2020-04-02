c4091d515a8d57dbebccddc5279529152afca32a
Mario Geier <marioegbertgeier@gmail.com>
2020-04-02 19:50:20 +0200

    Add display of camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,43 @@ import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 })
 export class HomePage implements OnInit {
 
-  constructor(private qrScanner: QRScanner) { }
+  constructor(private qrScanner: QRScanner,
+              public toastController: ToastController) { }
 
   ngOnInit() {
-    this.scannQRCode();
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
   }
 
   scannQRCode() {
@@ -20,25 +54,31 @@ export class HomePage implements OnInit {
       .then((status: QRScannerStatus) => {
         if (status.authorized) {
           // camera permission was granted
-
-
+          this.presentToast('connected');
+          this.qrScanner.show();
           // start scanning
           let scanSub = this.qrScanner.scan().subscribe((text: string) => {
             console.log('Scanned something', text);
+            this.presentToast('Scanned something' + text);
 
             this.qrScanner.hide(); // hide camera preview
             scanSub.unsubscribe(); // stop scanning
           });
 
         } else if (status.denied) {
+          this.presentToast('denied');
           // camera permission was permanently denied
           // you must use QRScanner.openSettings() method to guide the user to the settings page
           // then they can grant the permission from there
         } else {
           // permission was denied, but not permanently. You can ask for permission again at a later time.
+          this.presentToast('not permanently denied');
         }
       })
-      .catch((e: any) => console.log('Error is', e));
+      .catch((e: any) => {
+        console.log('Error is', e);
+        this.presentToast('Error: ' + e);
+      });
   }
 
 }
