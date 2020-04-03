1515fa9c8ce113898af612ad71391363195f0327
Mario Geier <marioegbertgeier@gmail.com>
2020-04-04 00:03:10 +0200

    Add age check

--- src/app/user-data/user-data.page.ts
+++ src/app/user-data/user-data.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-data',
@@ -10,15 +11,37 @@ import { MenuController } from '@ionic/angular';
 export class UserDataPage implements OnInit {
 
   constructor(private router: Router,
-    private menuCtrl: MenuController) {
+    private menuCtrl: MenuController,
+    private formBuilder: FormBuilder) {
     this.menuCtrl.enable(false);
   }
 
+  validations_form: FormGroup;
+
   ngOnInit() {
+
+    this.validations_form = this.formBuilder.group({
+      prename: new FormControl('', Validators.nullValidator),
+      name: new FormControl('', Validators.nullValidator),
+      phone: new FormControl('', Validators.nullValidator),
+      age: new FormControl('', Validators.min(1)),
+      residence: new FormControl('', Validators.nullValidator),
+    });
+  }
+
+  private navigateHome(): void {
+    this.router.navigate(['/home']);
   }
 
   applyData() {
-    this.router.navigate(['/home']);
+    if (this.validations_form.invalid) {
+      return;
+    }
+    this.navigateHome();
+  }
+
+  cancel() {
+    this.navigateHome();
   }
 
 }
