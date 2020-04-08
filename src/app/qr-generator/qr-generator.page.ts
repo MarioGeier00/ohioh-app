628eddfc7545c862809a77977763fd2480ced30a
Mario Geier <marioegbertgeier@gmail.com>
2020-04-08 16:48:31 +0200

    Add basic qr code scanning

--- src/app/qr-generator/qr-generator.page.ts
+++ src/app/qr-generator/qr-generator.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import QRCode from 'qrcode';
+
 @Component({
   selector: 'app-qr-generator',
   templateUrl: './qr-generator.page.html',
@@ -10,13 +12,22 @@ export class QrGeneratorPage implements OnInit {
   public name: string;
   public residence: string;
 
+  generated = '';
+  public displayQRCode: boolean;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   generateQRCode() {
-
+    const qrcode = QRCode;
+    const self = this;
+    qrcode.toDataURL(self.name, { errorCorrectionLevel: 'H' }, function (err, url) {
+      self.generated = url;
+    });
+    
+    this.displayQRCode = true;
   }
 
 }
