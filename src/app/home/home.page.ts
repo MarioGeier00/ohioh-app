647e7cbc135dbcac255a68f3d89fdd114ecc91f8
Mario Geier <marioegbertgeier@gmail.com>
2020-04-12 18:36:55 +0200

    Add ionic network

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MenuController, PopoverController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { PrototypeInfoComponent } from '../shared/prototype-info/prototype-info.component';
 import { UserService } from '../shared/data-services/user/user.service';
+import { Subscription } from 'rxjs';
+import { Network } from '@ionic-native/network/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
 
   public userDataAvailable: boolean;
 
+  private disconnectSubscription: Subscription;
+  private connectSubscription: Subscription;
+ 
   constructor(
     private menuCtrl: MenuController,
     private router: Router,
     public userService: UserService,
-    public popoverController: PopoverController
+    public popoverController: PopoverController,
+    private network: Network
   ) {
     this.menuCtrl.enable(true);
     this.userService.isUserDataEmpty()
@@ -25,6 +31,11 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit() {
+    this.setupNetworkConnectionCheck();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeNetworkConnectionCheck();
   }
 
   openQRScan() {
@@ -43,5 +54,39 @@ export class HomePage implements OnInit {
     });
     return await popover.present();
   }
+
+  setupNetworkConnectionCheck() {
+    // watch network for a disconnection
+    this.disconnectSubscription = this.network.onDisconnect().subscribe(() => {
+      console.log('network was disconnected :-(');
+    });
+
+    // watch network for a connection
+    this.connectSubscription = this.network.onConnect().subscribe(() => {
+      console.log('network connected!');
+      // We just got a connection but we need to wait briefly
+      // before we determine the connection type. Might need to wait.
+      // prior to doing any api requests as well.
+      setTimeout(() => {
+        if (this.network.type === 'wifi') {
+          console.log('we got a wifi connection, woohoo!');
+        }
+      }, 3000);
+    });
+
+  }
+
+  unsubscribeNetworkConnectionCheck() {
+    if (this.disconnectSubscription) {
+      // stop disconnect watch
+      this.disconnectSubscription.unsubscribe();
+    }
+
+    if (this.connectSubscription) {
+      // stop connect watch
+      this.connectSubscription.unsubscribe();
+    }
+  }
+
 
 }
