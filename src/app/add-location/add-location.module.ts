2966d13bc8657a4001691396c5a574416d0dc99c
Mario Geier <marioegbertgeier@gmail.com>
2020-04-25 12:08:55 +0200

    Add translation

--- src/app/add-location/add-location.module.ts
+++ src/app/add-location/add-location.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { AddLocationPageRoutingModule } from './add-location-routing.module';
 
 import { AddLocationPage } from './add-location.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { AddLocationPage } from './add-location.page';
     FormsModule,
     ReactiveFormsModule,
     IonicModule,
-    AddLocationPageRoutingModule
+    AddLocationPageRoutingModule,
+    TranslateModule.forChild()
   ],
   declarations: [AddLocationPage]
 })
