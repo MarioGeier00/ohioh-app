2e84bb4513d17db63e3cd072c6fb8ce8f4564a54
Mario Geier <marioegbertgeier@gmail.com>
2020-04-25 12:11:44 +0200

    Translate download started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -141,10 +141,10 @@ export class AppComponent implements OnInit {
   }
 
 
-  // downloadAPK() {
-  //   window.location.href = 'https://ohioh.azurewebsites.net/ohiohApp.apk';
-  //   // TODO: translation
-  //   this.presentToast('Download gestartet');
-  // }
+  downloadAPK() {
+    window.location.href = 'https://ohioh.azurewebsites.net/ohiohApp.apk';
+    // TODO: translation
+    this.presentToast(this.translation.get('downloadApp'));
+  }
 
 }
